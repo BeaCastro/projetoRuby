d60062144283770d27159d7be936fdc89355d766
EC2 Default User <ec2-user@ip-172-31-81-12.ec2.internal>
2019-11-11 14:04:05 +0000

    db

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -3,4 +3,11 @@ class MenuController < ApplicationController
 def teste
 end
 
+def root
+end
+
+def numero
+    @var = params[:x]
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :disciplinas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  root "menu#root"
   get "/teste", to: "menu#teste"
-  
+  get "/numero/:x", to: "menu#numero"
+  get "/cond/:nome", to: "pagina#cond"
 end
